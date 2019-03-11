5acb5339fa8d3a222bb844a74aecfa72a4209bcd
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 19:20:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  arra
+  array.any? |words|
+    words.length > 4
 end
 
 def find_the_cheese# code an argument here
