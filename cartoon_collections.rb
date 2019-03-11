66e8c4811e8c66b2ec5d3e89ca8d46b5333ce8cd
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 19:17:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,11 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  arr = []
   array.map do |element|
-    element.capitalize
+    arr << "#{element.capitalize}!" 
   end
-  return array 
+  return arr
 end
 
 def long_planeteer_calls# code an argument here
