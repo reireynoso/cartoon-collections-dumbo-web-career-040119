244517dfcee5e1255ec4058a944b800d33d4a5bd
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 19:09:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   i = 0
-  array.collect do |names|
-    i += 1
-    puts "#{i}. #{names}"
-  end
-  return array
+  array.each_index do |item|
 end
 
 def summon_captain_planet# code an argument here
