9ff19b42c3c69e85a8b52998b53a2f8327953dfd
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 19:13:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def roll_call_dwarves(array)# code an argument here
   return array
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  puts array
 end
 
 def long_planeteer_calls# code an argument here
